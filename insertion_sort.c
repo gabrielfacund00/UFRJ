e89c434be1127768c96033b09e75e7907ed5ab98
Igor Victorino <121045757+OGalindoGeorge@users.noreply.github.com>
2025-10-22 13:19:19 -0300

    Add insertion sort function in insertion_sort.c
    Implement insertion sort algorithm in C.

---
+++ insertion_sort.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void insertionSort ( int arr [] , int n ) {
+  for (int i = 1; i < n ; i ++) {
+  int key = arr [ i ];
+  int j = i - 1;
+  // Move os elementos maiores que a key para a
+  frente
+  while ( j >= 0 && arr [ j ] > key ) {
+    arr [ j + 1] = arr [ j ];
+    j = j - 1;
+    }
+  arr [ j + 1] = key ;
+  }
+  }
+}
